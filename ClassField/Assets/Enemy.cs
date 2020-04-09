842a1c089b01c792b58ba61396dde00bf29e5db6
xiaoshane0408 <63327406+xiaoshane0408@users.noreply.github.com>
2020-04-09 10:28:47 +0800

    done

--- ClassField/Assets/Enemy.cs
+++ ClassField/Assets/Enemy.cs
@@ -1,6 +1,40 @@
 ﻿using UnityEngine;
+/// <summary>
+/// 定義怪物物件
+/// </summary>
 
 public class Enemy : MonoBehaviour
 {
-
+    /// <summary>
+    /// HP
+    /// </summary>
+    [Header("血量"),Tooltip("怪物的血量"),Range(50,500)]
+    public int hp;
+    /// <summary>
+    /// MP
+    /// </summary>
+    [Header("魔力"), Tooltip("怪物的魔力"), Range(10, 100)]
+    public int mp = 100;
+    /// <summary>
+    /// 能力值
+    /// </summary>
+    [Header("能力值"), Range(1, 50.5f)]
+    public float speed = 10.5f;
+    [Range(20,200)]
+    public int atk = 50;
+    [Range(0, 100)]
+    public int def;
+    /// <summary>
+    /// 裝備
+    /// </summary>
+    [Header("裝備")]
+    public string weapon;
+    public string equipment = "皮衣";
+    /// <summary>
+    /// 其他資料
+    /// </summary>
+    [Header("其他資料"), Tooltip("怪物是否帶有鑰匙")]
+    public bool key;
+    [Tooltip("怪物是否帶有寶物")]
+    public bool treasure;
 }
---
+++ ClassField/Assets/EnemyManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Enemy enemy1;
+    public Enemy enemy2;
+
+    private void Start()
+    {
+        // get
+        print("怪物一號的魔力 : " + enemy1.mp);
+        print("怪物二號的裝備名稱 : " + enemy2.equipment);
+
+        // set
+        enemy1.hp = 100;
+        enemy2.hp = 300;
+        enemy1.def = 50;
+        enemy1.weapon = "木棍";
+        enemy2.weapon = "短刀";
+        enemy1.key = true;
+        enemy2.treasure = true;
+    }
+    
+}
